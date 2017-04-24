3e7c2bf357cc42ae086d15d3e0b299be95bb8e0a
cwza <cwz0205a@gmail.com>
2017-04-24 18:09:32 +0800

    change something

---
+++ setup.sh
@@ -0,0 +1,9 @@
+gcloud compute disks create --size=10GB \
+  'redis-1' 'redis-2' 'redis-3' \
+  'redis-4' 'redis-5' 'redis-6'
+
+kubectl create configmap redis-conf --from-file=redis.conf
+
+kubectl create -f deployment
+
+kubectl create -f services
